42d6cd45986ad396a064b3a85190ec06de9e355d
DarshanReddy34 <mundlahemadarshan.reddy@wissen.com>
2021-05-19 20:39:13 +0530

    Initial commit

---
+++ functions/QuickAnswers.js
@@ -0,0 +1,18 @@
+const TokenValidator = require('twilio-flex-token-validator').functionValidator;
+exports.handler = TokenValidator(function(context, event, callback) {
+  
+    const fs = require('fs');
+    const fileName = '/quickanswers.json';
+    const response = new Twilio.Response();
+    response.appendHeader('Access-Control-Allow-Origin', '*');
+    response.appendHeader('Access-Control-Allow-Methods', 'POST, GET');
+    response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+    response.appendHeader('Content-Type', 'application/json');
+    let file = Runtime.getAssets()[fileName].path;
+    let data = fs.readFileSync(file);
+    let quickanswersList = JSON.parse(data);
+    response.setBody(quickanswersList);
+    console.log("quickanswers list:  ###", quickanswersList);
+  
+    callback(null, response);
+});
